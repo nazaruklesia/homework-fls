6381f432f47aa548109710e7ab65ffaeee7a9278
Lesia_Nazaruk <nazarullesia@gmail.com>
2025-06-25 15:26:33 +0300

    add media

--- lessons_js/js/script.js
+++ lessons_js/js/script.js
@@ -1,3 +1,4 @@
+"use strict";
 // const somePoints = 0;
 
 // for (let i = 0; i <= 10; ++i) {
@@ -14,33 +15,22 @@ if (2 * 20 <= 10 || (30 / 2 < 5 && 10 <= "10") || 20 === "20") {
 // Викличте функцію передаючі різні значення, у тому числі не передаючи зовсім.
 // Функція не має повертати NaN, Infinite або помилку
 
-// const calcDivision = (a, b) => {
-//   if (
-//     a !== 0 &&
-//     b !== 0 &&
-//     typeof a === "number" &&
-//     !Number.isNaN(a) &&
-//     typeof b === "number" &&
-//     !Number.isNaN(b)
-//   ) {
-//     const resultDivision = a / b;
-//     console.log(`Результат ділення: ${resultDivision}`);
-//   } else if (a === undefined || b === undefined) {
-//     console.log(`Результат ділення: потрібно ввести дані`);
-//   } else {
-//     console.log(`Результат ділення: упс, щось пішло не так`);
-//   }
-// };
-// calcDivision(20, 10);
-// calcDivision(20, "10");
-// calcDivision(100, 0);
-// calcDivision(NaN, 3);
-// calcDivision(3, 2);
-// calcDivision("Text", "text");
-// calcDivision(2, 110);
-// calcDivision("10", 2);
-// calcDivision();
-// calcDivision(2);
+const calcDivision = (a, b) => {
+  let rezult = a / b;
+  if (isFinite(rezult) && !isNaN(rezult)) {
+    return console.log(`Результат ділення: ${rezult}`);
+  }
+};
+calcDivision(20, 10);
+calcDivision(20, "10");
+calcDivision(100, 0);
+calcDivision(NaN, 3);
+calcDivision(3, 2);
+calcDivision("Text", "text");
+calcDivision(2, 110);
+calcDivision("10", 2);
+calcDivision();
+calcDivision(2);
 
 //Створіть масив даних - 5 елементів, один з яких число 10
 //Обробіть масив за допомогою методу перебору

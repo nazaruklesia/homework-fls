a9c35bdba8e5dc20f3c923b3d4ecde25a0390554
Lesia_Nazaruk <nazarullesia@gmail.com>
2025-06-25 23:21:30 +0300

    add media and fixed css

---
+++ lesson_16/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("burger-btn").addEventListener("click", () => {
+  document.getElementById("nav-list").classList.toggle("active");
+  document.body.classList.toggle("lock");
+});
